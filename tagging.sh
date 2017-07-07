bc19a7d0fed5f6e0c94de677c286b3e22d79b086
Daniel Lobato Garcia <me@daniellobato.me>
2017-07-07 16:53:52 +0200

    Fix typos in tagging.sh

--- tagging.sh
+++ tagging.sh
@@ -5,14 +5,19 @@ PATCH=`cat VERSION | cut -d. -f3`
 
 RELEASEDIR=`pwd`
 
+# Check for 'tx', '~/.transifexrc', 'jq'
 # foreman
-git clone theforeman/foreman
+git clone git@github.com:theforeman/foreman.git
 cd foreman
 git checkout $MAJOR.$MINOR-stable
+echo "gem 'rdoc'" > bundler.d/Gemfile.local.rb
+bundle install
+cp config/settings.yaml.example config/settings.yaml
+cp config/database.yml.example config/database.yml
 make -C locale tx-update
 script/sync_templates.sh
 cp "$RELEASEDIR"/VERSION .
-tmp=$(mktmp)
+tmp=$(mktemp)
 jq ".version = \"$VERSION\"" package.json > "$tmp" && mv "$tmp" package.json
 extras/changelog
 git add .
@@ -22,11 +27,11 @@ git push origin $MAJOR.$MINOR-stable
 git push origin $VERSION
 
 # foreman-proxy
-git clone theforeman/smart-proxy
+git clone git@github.com:theforeman/smart-proxy.git
 cd smart-proxy
 git checkout $MAJOR.$MINOR-stable
 cp "$RELEASEDIR"/VERSION .
-extras/changelog
+extra/changelog
 git add .
 git commit -m "Release $VERSION"
 git tag -m "Release $VERSION" $VERSION
@@ -34,7 +39,7 @@ git push origin $MAJOR.$MINOR-stable
 git push origin $VERSION
 
 # foreman-installer
-git clone theforeman/foreman-installer
+git clone git@github.com:theforeman/foreman-installer.git
 cd foreman-installer
 git checkout $MAJOR.$MINOR-stable
 cp "$RELEASEDIR"/VERSION .
@@ -45,7 +50,7 @@ git push origin $MAJOR.$MINOR-stable
 git push origin $VERSION
 
 # foreman-selinux
-git clone theforeman/foreman-selinux
+git clone git@github.com:theforeman/foreman-selinux.git
 cd foreman-selinux
 git checkout $MAJOR.$MINOR-stable
 cp "$RELEASEDIR"/VERSION .
